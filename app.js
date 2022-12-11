b4ddb0543308395262e06a8492a5ff67c469ba8b
entrydenied <benjamintak17@gmail.com>
2022-12-11 15:18:26 -0800

    Remove code from book function

--- app.js
+++ app.js
@@ -2,23 +2,13 @@
 let myLibrary = [];
 
 
-// Makes books
+// Book constructor
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    
-    this.info = function() {
-      return(title + " by " + author + ", " + pages + ", " + read)
-    }
   }
-  
-  const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
-  const theHobber = new Book('The Hobber', 'J.R.E. Experience', '321 pages', 'read')
-  
-  console.log(theHobbit.info());
-  myLibrary.push(theHobbit, theHobber);
 
   // Adds books to the Library
   function addBookToLibrary() {
@@ -32,4 +22,19 @@ function availableBooks(array) {
     console.table(myLibrary);
 }
 
+  
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
+const theHobber = new Book('The Hobber', 'J.R.E. Experience', '321 pages', 'read')
+
+console.log(theHobbit.info());
+myLibrary.push(theHobbit, theHobber);
+
+
 availableBooks(myLibrary);
+
+
+
+    
+// this.info = function() {
+//   return(title + " by " + author + ", " + pages + ", " + read)
+// }
