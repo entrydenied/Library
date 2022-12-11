12ba6d5a09a4b650d48d080a1c5dd28d57fd09d0
entrydenied <benjamintak17@gmail.com>
2022-12-11 15:43:43 -0800

    Add global definitions & change const to let

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
+// Global Definitions
+let title = document.getElementById('title');
+let author = document.getElementById('author');
+let pages = document.getElementById('pages');
+let read = document.getElementById('read');
+
+
 // Holds books
 let myLibrary = [];
 
 
 // Book constructor
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
   }
 
-  // Adds books to the Library
+  // 2. Add a function to the script that can take user’s input and store the new book objects into an array.
   function addBookToLibrary() {
+    let newBook = new Book (bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value)
+    // let usersBook = prompt()
+    myLibrary.push(newBook)
 
   }
 
   // 3. Write a function that loops through the array and displays each book on the page
-
-
 function availableBooks(array) {
     console.table(myLibrary);
 }
 
   
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
-const theHobber = new Book('The Hobber', 'J.R.E. Experience', '321 pages', 'read')
 
-console.log(theHobbit.info());
+
+
+let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
+let theHobber = new Book('The Hobber', 'J.R.E. Experience', '321 pages', 'read')
+
+console.log(theHobbit);
 myLibrary.push(theHobbit, theHobber);
 
 
