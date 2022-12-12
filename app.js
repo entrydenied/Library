e9ac3aca806c68198e0de9d395cec02da4efc44e
entrydenied <benjamintak17@gmail.com>
2022-12-11 16:57:06 -0800

    Add makeLibrary & displayBooks

--- app.js
+++ app.js
@@ -17,21 +17,53 @@ function Book(title, author, pages, read) {
     this.read = read;
   }
 
-  // 2. Add a function to the script that can take user’s input and store the new book objects into an array.
-  function addBookToLibrary() {
-    let newBook = new Book (bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value)
-    // let usersBook = prompt()
-    myLibrary.push(newBook)
-
-  }
-
   // 3. Write a function that loops through the array and displays each book on the page
-function availableBooks(array) {
-    console.table(myLibrary);
+
+function makeLibrary () {
+    for (let i = 0; i <= myLibrary.length -1; i++) {
+        displayBooks(makeLibrary[i])
+    }
+    function displayBooks() {
+        let bookContainer = document.createElement('li')
+        bookContainer.classList.add('item-container')
+
+        let titleValue = document.createElement('h5')
+        titleValue.classList.add('book-title');
+		titleValue.id = 'title-cont';
+		titleValue.textContent = myLibrary[i].title;
+
+        let authorValue = document.createElement('h5');
+		authorValue.classList.add('book-author');
+		authorValue.id = 'author-cont';
+		authorValue.textContent = myLibrary[i].author;
+
+        let pagesValue = document.createElement('h5');
+		pagesValue.classList.add('book-pages');
+		pagesValue.id = 'pages-cont';
+		pagesValue.textContent = myLibrary[i].pages;
+
+        let readValue = document.createElement('h5');
+		readValue.classList.add('book-read');
+		readValue.id = 'pages-cont';
+		readValue.textContent = myLibrary[i].pages;
+
+        container.appendChild(bookContainer);
+		bookContainer.appendChild(titleValue);
+		bookContainer.appendChild(authorValue);
+		bookContainer.appendChild(pagesValue);
+		bookContainer.appendChild(readValue);
+    }
 }
 
-  
 
+
+
+    // 2. Add a function to the script that can take user’s input and store the new book objects into an array.
+function addBookToLibrary() {
+    let newBook = new Book (bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value)
+    // let usersBook = prompt()
+    myLibrary.push(newBook)   
+}
 
 
 let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
