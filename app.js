34461d2cb10cce93687006f60700202db70b6abd
entrydenied <benjamintak17@gmail.com>
2022-12-11 17:12:54 -0800

    Move & fix addBookToLibrary

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let title = document.getElementById('title');
 let author = document.getElementById('author');
 let pages = document.getElementById('pages');
 let read = document.getElementById('read');
+let container = document.getElementById('container')
 
 
 // Holds books
@@ -17,12 +18,27 @@ function Book(title, author, pages, read) {
     this.read = read;
   }
 
+// 2. Add a function to the script that can take user’s input and store the new book objects into an array.
+function addBookToLibrary() {
+    myLibrary.push(this)   
+}
+
+
+let book1 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'unread')
+let book2 = new Book('The Hobber', 'J.R.E. Experience', '321', 'read')
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+  
   // 3. Write a function that loops through the array and displays each book on the page
 
 function makeLibrary () {
-    for (let i = 0; i <= myLibrary.length -1; i++) {
-        displayBooks(makeLibrary[i])
-    }
+    let i = 0;
+	do {
+		displayBooks(myLibrary[i])
+		i++;
+	} while (i <= myLibrary.length - 1)
+
     function displayBooks() {
         let bookContainer = document.createElement('li')
         bookContainer.classList.add('item-container')
@@ -55,29 +71,14 @@ function makeLibrary () {
     }
 }
 
+makeLibrary();
 
+// availableBooks(myLibrary);
 
+// let newBook = new Book (bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value)
+// let usersBook = prompt()
 
-    // 2. Add a function to the script that can take user’s input and store the new book objects into an array.
-function addBookToLibrary() {
-    let newBook = new Book (bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value)
-    // let usersBook = prompt()
-    myLibrary.push(newBook)   
-}
-
-
-let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
-let theHobber = new Book('The Hobber', 'J.R.E. Experience', '321 pages', 'read')
-
-console.log(theHobbit);
-myLibrary.push(theHobbit, theHobber);
-
-
-availableBooks(myLibrary);
-
-
-
-    
+ 
 // this.info = function() {
 //   return(title + " by " + author + ", " + pages + ", " + read)
 // }
